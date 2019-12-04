f30460edf0d0d849f65b71ed66f61b64e1b65300
kauedesousa <desousa.kaue@gmail.com>
2019-12-04 13:37:04 +0100

    get

--- R/get_chirps.R
+++ R/get_chirps.R
@@ -1,38 +1,65 @@
-#' Get CHIRPS
+#' Get CHIRPS precipitation data
 #' 
 #' Fetch the Climate Hazards Group InfraRed Precipitation with Station Data via 
-#' ClimateSERV API Client
+#' ClimateSERV API Client. ClimateSERV works with geojson strings of type 'Polygon'. The 
+#' input 'lonlat' are then transformed into polygons with a small buffer area around 
+#' the point.
 #' 
 #' @param lonlat a data.frame of geographic coordinates of longitude and latitude in that order
 #' @param dates a character of start and end dates in that order in the format YYYY-MM-DD
-#' @param operation optional, an integer that represents which type of statistical operation to perform on the dataset
+#' @param operation optional, an integer that represents which type of statistical operation 
+#' to perform on the dataset
 #' @details Types of operation supported by ClimateSERV are,
-#' max = 0, min = 1, median = 2, range = 3, sum = 4, average = 5
-#' @return A data frame of CHIRPS data including latlon and dates
-#' @seealso \url{https://climateserv.servirglobal.net} and \url{http://chg.geog.ucsb.edu/data/chirps/} 
-#' @examples 
-#' lonlat <- data.frame(lon = runif(4, 5, 7),
-#'                      lat = runif(4, 35, 37))
-#' dates <- c("2018-10-01","2018-11-01")
+#' max = 0, min = 1, median = 2, sum = 4, average = 5
+#' @return A data frame of CHIRPS data including:
+#' \item{id}{the index for the rows in \code{lonlat}}
+#' \item{dates}{the dates from which CHIRPS was requested}
+#' \item{lon}{the longitude as provided in \code{lonlat}}
+#' \item{lat}{the latitude as provided in \code{lonlat}}
+#' \item{chirps}{the CHIRPS value in mm}
+#' @seealso ClimateSERV (\link{https://climateserv.servirglobal.net})
+#' @references 
+#' 
+#' Funk C. et al. (2015). Scientific Data, 2, 150066. \url{https://doi.org/10.1038/sdata.2015.66}
+#' 
+#' @examples
+#' \donttest{
+#' lonlat <- data.frame(lon = runif(2, -55, -54),
+#'                      lat = runif(2, -3, -2.7))
+#' 
+#' dates <- c("2017-12-15","2018-01-20")
+#' 
 #' 
 #' df <- get_chirps(lonlat, dates)
+#' } 
 #' @importFrom httr accept_json content GET
 #' @importFrom jsonlite fromJSON
 #' @importFrom sf st_point st_buffer
-#' @importFrom tibble as_tibble tibble
+#' @importFrom tibble as_tibble
 #' @export
 get_chirps <- function(lonlat, dates, operation = 5) {
   
 
+  # the first path is the request the data
+  # a successful request returns an id from which is possible to download the data
   path <- "https://climateserv.servirglobal.net/chirps/submitDataRequest/?"
   
+  # the second path is to download the data from the previous request
+  # a successful returns a json file which can be transformed into a data.frame 
   path2 <- "https://climateserv.servirglobal.net/chirps/getDataFromRequest/?"
   
   nr <- nrow(lonlat)
   
-  lonlat <- .c_polygon(lonlat)
+  # validate lonlat to check if they are within the CHIRPS range lat -50, 50
+  .validate_lonlat(lonlat, silent = TRUE)
   
-  # format for ClimateSERV is ("MM/DD/YYYY")
+  # validate dates
+  .validate_dates(dates, silent = TRUE)
+  
+  # get geojson strings from lonlat
+  gjson <- .c_polygon(lonlat)
+  
+  # reformat dates as required in ClimateSERV ("MM/DD/YYYY")
   begindate <- dates[1]
   begindate <- strsplit(begindate, "-")[[1]]
   begindate <- paste(begindate[2], begindate[3], begindate[1], sep = "/")
@@ -41,30 +68,33 @@ get_chirps <- function(lonlat, dates, operation = 5) {
   enddate <- strsplit(enddate, "-")[[1]]
   enddate <- paste(enddate[2], enddate[3], enddate[1], sep = "/")
   
+  # force a string in operation integer
+  operation <- toString(operation)
   
   # submit data request
-  ids <- 
-    lapply(lonlat, function(x) {
-      
-      query <- list(
-        datatype = "0",
-        begintime =  "04/01/2018",
-        endtime = "04/30/2018",
-        intervaltype = "0",
-        operationtype = "5",
-        callback = "successCallback",
-        dateType_Category = "default",
-        isZip_CurrentDataType = "false",
-        geometry = x
-      )
-      
-      id <- httr::GET(url = path, 
-                      query = query)
-      
-      id <- httr::content(id, as = "text", encoding = "UTF-8")
-      
-    })
-  
+  ids <- lapply(gjson, function(x) {
+    
+    # organise the query
+    query <- list(
+      datatype = "0",
+      begintime =  begindate,
+      endtime = enddate,
+      intervaltype = "0",
+      operationtype = operation,
+      callback = "successCallback",
+      dateType_Category = "default",
+      isZip_CurrentDataType = "false",
+      geometry = x
+    )
+    
+    # sent the query
+    id <- httr::GET(url = path, 
+                    query = query)
+    
+    # get content from the query
+    id <- httr::content(id, as = "text", encoding = "UTF-8")
+    
+  })
   
   # check that all calls were successful
   success <- lapply(ids, function(x) {
@@ -93,7 +123,7 @@ get_chirps <- function(lonlat, dates, operation = 5) {
   })
   
   # get data from request
-  data <- lapply(ids, function(x) {
+  result <- lapply(ids, function(x) {
     
     d <- httr::GET(url = path2,
                    query = list(id = x),
@@ -103,26 +133,45 @@ get_chirps <- function(lonlat, dates, operation = 5) {
     
     d <- jsonlite::fromJSON(d)
     
-    d <- data.frame(date = d$data$date,
-                    chirps = d$data$value$avg, 
-                    stringsAsFactors = FALSE)
+    d <- data.frame(cbind(date = d$data$date,
+                          d$data$value))
+    
+    d$date <- as.character(d$date)
     
     return(d)
     
   })
   
-  data <- do.call("rbind", data) 
+  result <- do.call("rbind", result) 
   
-  dat <-  strsplit(data$date, "/")
-  dat <- do.call(rbind, dat)
+  # fix ids
+  id <- strsplit(row.names(result), "[.]")
+  id <- do.call("rbind", id)[,1]
+  result$id <- id
+  
+  # transform dates to the original format as input
+  dat <-  strsplit(result$date, "/")
+  dat <- do.call("rbind", dat)
   dat <- paste(dat[,3], dat[,1], dat[,2], sep = "-")
-  data$date <- as.Date(dat, format = "%Y-%m-%d")
+  result$date <- as.Date(dat, format = "%Y-%m-%d")
   
-  data$id <- rep(1:nr, each = length(unique(dat)))
+  lonlat$id <- rownames(lonlat)
   
-  data <- tibble::as_tibble(data)
+  result <- merge(result, lonlat, by = "id")
   
-  data <- data[c(4,1,2,3)]
+  if (operation == "3") {
+    
+  }
   
-  return(data)
+  if (operation != "3") {
+    
+    names(result)[3:5] <- c("chirps","lon","lat")
+    result <- result[, c("id","lon","lat","date","chirps")]
+  
+  }
+  
+  result <- tibble::as_tibble(result)
+  
+  return(result)
 }
+
--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,6 +1,7 @@
-# Concatenate a character in the format of geojson of type Polygon for the API call
+# Concatenate lonlat into a geojson string of type Polygon for the API call
+# 
 # @param geometry a matrix for polygon geometry (lonlat)
-# @return a character with the structure of geojson
+# @return A geojson string of type 'Polygon' 
 # @examples
 # lonlat <- matrix(c(30,20,
 #                    45,40,
@@ -9,6 +10,7 @@
 # 
 # gj <- .c_geojson(lonlat)
 # plot(sf::read_sf(gj))
+# 
 .c_geojson <- function(geometry) {
   
   geometry <- paste(geometry[,1], geometry[,2], sep = ",", collapse = "],[")
@@ -19,14 +21,24 @@
   
 }
 
-
-# random geographic locations around bbox(10, 12, 55, 57)
+# Concatenate a coordinate point into a geojson polygon
+#
+# Take single points from geographical coordinates 
+# and convert it into a geojson 'Polygon' string using sf::st_buffer
+# 'Polygon' is the only geojson accepted by ClimateSERV
+#
+# @param lonlat a data.frame with geographical coordinates lonlat in that order
+# @param dist numeric; buffer distance for all \code{lonlat}
+# @return A list with geojson strings for each row in \code{lonlat}
+# @examples
+# # random geographic locations around bbox(10, 12, 45, 57)
 # set.seed(123)
 # lonlat <- data.frame(lon = runif(10, 10, 12),
-#                      lat = runif(10, 55, 57))
+#                      lat = runif(10, 45, 57))
 # 
 # .c_polygon(lonlat)
-.c_polygon <- function(lonlat) {
+# 
+.c_polygon <- function(lonlat, dist = 0.00001) {
   
   lonlat <- split(lonlat, 1:nrow(lonlat))
   
@@ -35,7 +47,7 @@
     
     x <- sf::st_point(x)
     
-    x <- sf::st_buffer(x, 0.00001)
+    x <- sf::st_buffer(x, dist)
     
     x <- x[[1]]
     
@@ -46,6 +58,78 @@
   return(geo)
     
 }
+
+
+# Validate lonlat within an accepted geographical range
+# 
+# @param lonlat a data.frame with geographical coordinates lonlat in that order
+# @param xlim a numeric vector for the min and max accepted range in the longitude in that order
+# @param ylim a numeric vector for the min and max accepted range in the latitude in that order 
+# @param silent logical, return messages via \code{cat}
+# @return nothing
+# @examples
+# # random geographic locations around bbox(10, 12, 45, 57)
+# set.seed(123)
+# lonlat <- data.frame(lon = runif(10, 10, 12),
+#                      lat = runif(10, 45, 49))
+# 
+# .validate_lonlat(lonlat)
+
+.validate_lonlat <- function(lonlat, xlim = c(-180, 180), ylim = c(-50, 50), silent = FALSE) {
+  
+  lon <- lonlat[,1]
+  
+  lat <- lonlat[,2]
+  
+  v1 <- min(lon) < xlim[1]
+  v2 <- max(lon) > xlim[2]
+  v3 <- min(lat) < ylim[1]
+  v4 <- max(lat) > ylim[2]
+  
+  if(any(c(v1,v2,v3,v4))) {
+    stop("lonlat are beyond the accepted lims, which are: " ,
+         paste(paste(xlim[1], ylim[1], sep = " , "),
+               paste(xlim[2], ylim[2], sep = " , "), sep = " , "))
+  }
+  if (!silent) {
+    return(cat("lonlat OK! \n"))
+  } 
+}
+
+
+# Validate dates within an accepted range
+#
+# @param x a character of start and end dates in that order in the format YYYY-MM-DD
+# @param silent logical, return messages via \code{cat}
+# @return nothing
+# @examples
+# dates <- c("2016-01-31","2017-12-01")
+# 
+# .validate_dates(dates)
+# 
+# 
+# dates <- c("2018-01-31","2017-12-01")
+# 
+# .validate_dates(dates)
+.validate_dates <- function(x, silent = FALSE) {
+  
+  xmin <- as.Date(x[1], format = "%Y-%m-%d")
+  
+  xmax <- as.Date(x[2], format = "%Y-%m-%d")
+  
+  accepted <- as.integer(xmax - xmin) > 1
+  
+  if(accepted & !silent) {
+    cat("dates OK! \n")
+  }
+  
+  if(!accepted) {
+    stop("something wrong with dates provided, end date seems to be older than begin date \n")
+  }
+  
+}
+
+
 
 
 
