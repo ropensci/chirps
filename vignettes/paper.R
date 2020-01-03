1927f5a7a844d6ea6fd0aaee64625d7d4b5e1787
Kauê de Sousa <desousa.kaue@gmail.com>
2020-01-03 15:43:45 +0100

    paper-first-draft

--- vignettes/paper.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-TRAVIS <- !identical(tolower(Sys.getenv("TRAVIS")), "true")
-knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>",
-  purl = TRAVIS
-)
-
