39f345033155ff7642ccf95b6199e879f13be714
Adam H. Sparks <adamhsparks@gmail.com>
2020-01-05 21:23:37 +1000

    Add package document file

---
+++ R/chirps.R
@@ -0,0 +1,16 @@
+
+#' API Client for CHIRPS
+#'
+#' @name chirps
+#' @author Kauê de Sousa and Adam H. Sparks and William Ashmall and Jacob van
+#'  Etten and Svein Ø. Solberg
+#' @docType package
+#'
+#' @seealso
+#'\strong{Useful links:}
+#' \itemize{
+#' \item{Development repository: \url{https://github.com/agrobioinfoservices/chirps}}
+#' \item{Static documentation: \url{https://agrobioinfoservices.github.io/chirps/}}
+#' \item{Report bugs: \url{https://github.com/agrobioinfoservices/chirps/issues}}
+#' }
+"_PACKAGE"
