61a4f5829395ff5c2ce42c824cdfbfc2837594a5
Adam H. Sparks <adamhsparks@gmail.com>
2020-02-29 08:32:50 +1000

    Enable VCR for tests requiring Internet connectivity

--- tests/testthat/test-get_chirps.R
+++ tests/testthat/test-get_chirps.R
@@ -4,7 +4,7 @@ context("test-get_chirps")
 load("../test_data.rda")
 
 # Test if get_chirps fetch the correct values,
-# for this we downloaded two points from 
+# for this we downloaded two points from
 # https://climateserv.servirglobal.net/
 # and will compare it with the values retrieved by get_chirps
 
@@ -12,8 +12,6 @@ load("../test_data.rda")
 
 test_that("default method", {
   vcr::use_cassette("default_method", {
-    skip_on_cran()
-    
     x <- get_chirps(lonlat, dates)
     
     x <- round(x$chirps, 2)
@@ -26,35 +24,32 @@ test_that("default method", {
 
 library("sf")
 # get_chirps for sf objects
-coords <- st_as_sf(lonlat, coords = c("lon","lat"))
+coords <- st_as_sf(lonlat, coords = c("lon", "lat"))
 
 test_that("sf method", {
-  skip_on_cran()
-  
-  y <- get_chirps(coords, dates)
-  
-  y <- round(y$chirps, 2)
-  
-  equal <- all(y == chirps$chirps)
-
-  expect_true(equal)
-
+  vcr::use_cassette("sf_method", {
+    y <- get_chirps(coords, dates)
+    
+    y <- round(y$chirps, 2)
+    
+    equal <- all(y == chirps$chirps)
+    
+    expect_true(equal)
+  })
 })
 
 # get chirps with geojson method
 geojson <- dataframe_to_geojson(lonlat)
 
+
 test_that("geojson method", {
-  skip_on_cran()
-  
-  z <- suppressWarnings(
-    get_chirps(geojson, dates)
-    )
-
-  z <- round(z$chirps, 2)
-  
-  equal <- all(z == chirps$chirps)
-
-  expect_true(equal)
+  vcr::use_cassette("geojson_method", {
+    z <- suppressWarnings(get_chirps(geojson, dates))
     
+    z <- round(z$chirps, 2)
+    
+    equal <- all(z == chirps$chirps)
+    
+    expect_true(equal)
+  })
 })
--- tests/testthat/test-get_esi.R
+++ tests/testthat/test-get_esi.R
@@ -4,22 +4,21 @@ context("test-get_esi")
 load("../test_data.rda")
 
 # Test if get_chirps fetch the correct values,
-# for this we downloaded two points from 
+# for this we downloaded two points from
 # https://climateserv.servirglobal.net/
 # and will compare it with the values retrieved by get_chirps
 
 values <- c(NA, -1.89, -1.92, -2.12)
 
-# Test default method
-test_that("default method", {
-  skip_on_cran()
-  
-  x <- get_esi(lonlat, dates = c("2002-01-01", "2002-01-31"))
-  
-  x <- round(x$esi, 2)
-  
-  equal <- all(x == values, na.rm = TRUE)
-  
-  expect_true(equal)
-  
+# Test get_esi
+test_that("get_esi", {
+  vcr::use_cassette("get_esi", {
+    x <- get_esi(lonlat, dates = c("2002-01-01", "2002-01-31"))
+    
+    x <- round(x$esi, 2)
+    
+    equal <- all(x == values, na.rm = TRUE)
+    
+    expect_true(equal)
+  })
 })
