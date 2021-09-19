6daaa04ff5a58e20887261e4b6d7524caa0990cc
Adam Sparks <adamhsparks@icloud.com>
2021-09-19 10:10:45 +0800

    Add new cassette for get_esi tests

--- tests/testthat/test-get_esi.R
+++ tests/testthat/test-get_esi.R
@@ -1,11 +1,12 @@
-# load("tests/test_data.rda")
+
 load("../test_data.rda")
 
 # Test get_esi() -----
-test_that("get_esi", {
-  x <- get_esi(lonlat, dates = c("2002-01-01", "2002-01-31"))
-  
+test_that("get_esi returns proper values", {
+  vcr::use_cassette("get_esi", {
+    x <- get_esi(lonlat, dates = c("2002-01-01", "2002-01-31"))
+  })
   expect_named(x, c("id", "lon", "lat", "date", "esi"))
-  expect_equal(nrow(x), 10)
+  expect_equal(nrow(x), 4)
   expect_s3_class(x, c("chirps", "chirps_df", "data.frame"))
 })
