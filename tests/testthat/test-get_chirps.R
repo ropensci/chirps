b6d5f08cce828eb791705d5e26550f8477fe0ffa
Adam Sparks <adamhsparks@icloud.com>
2021-09-19 13:37:02 +0800

    More fiddling with vcr for tests

--- tests/testthat/test-get_chirps.R
+++ tests/testthat/test-get_chirps.R
@@ -1,4 +1,5 @@
-# test_data.rda contains lat/lon and date values for the following test
+
+# test_data.rda contains lat/lon and date values for the following tests
 load("../test_data.rda")
 
 # Test get_chirps() -----
@@ -8,10 +9,10 @@ test_that("get_chirps() returns proper values", {
   })
   expect_named(x, c("id", "lon", "lat", "date", "chirps"))
   expect_equal(nrow(x), 10)
-  expect_s3_class(x, c("chirps_df", "data.frame"))
+  expect_s3_class(x, c("chirps", "chirps_df", "data.frame"))
 })
 
-# Test sf data frame method -----
+# Test sf return data frame method -----
 coords <- st_as_sf(lonlat, coords = c("lon", "lat"))
 test_that("get_chirps() sf method return df", {
   vcr::use_cassette("CHIRPS_sf_method_return_df", {
@@ -19,5 +20,20 @@ test_that("get_chirps() sf method return df", {
   })
   expect_named(x, c("id", "lon", "lat", "date", "chirps"))
   expect_equal(nrow(x), 10)
-  expect_s3_class(x, c("chirps_df", "data.frame"))
+  expect_s3_class(x, c("chirps", "chirps_df", "data.frame"))
+})
+
+# Test sf return `sf` method -----
+test_that("get_chirps() sf method return sf", {
+  vcr::use_cassette("CHIRPS_sf_method_return_sf", {
+    x <- get_chirps(coords, dates, as.sf = TRUE)
+  })
+  expect_named(x, c("day_10957",
+                    "day_10958",
+                    "day_10959",
+                    "day_10960",
+                    "day_10961",
+                    "geometry"))
+  expect_equal(nrow(x), 2)
+  expect_s3_class(x, c("sf", "data.frame"))
 })
